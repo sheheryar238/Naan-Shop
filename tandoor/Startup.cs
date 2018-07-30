41bf05e2cc7faa47e1e39460e1463113d5204a9e
Sheheryar Ali <34582497+sheheryar238@users.noreply.github.com>
2018-07-30 21:30:55 +0500

    errors resolved

---
+++ tandoor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tandoor.Startup))]
+namespace tandoor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
